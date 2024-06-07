2a97e35cc9d50ca94a83ece15c5c1086259068e4
zabooz <zabooz1988@gmail.com>
2024-06-07 23:47:08 +0200

    fixed modules

--- client/bruteLibrary.js
+++ client/bruteLibrary.js
@@ -1,3 +1,23 @@
+const fs = require('fs')
+
+
+fs.readFile('rockYou.json','utf-8',(err,data)=> {
+    if(err){
+        console.log(err)
+        return
+    }
+
+
+    let passwords = JSON.parse(data)
+
+    console.log(passwords)
+
+
+})
+
+
+
+
 export function bruteForceLibrary(){
 
     
--- client/main.js
+++ client/main.js
@@ -1,8 +1,8 @@
-import {generatePassword} from 'passwordGenerator.js'
-import {generateConverter} from 'passwordConverter,js'
-import {bruteForceSimple} from 'bruteSimple.js'
-import {bruteForceLibrary} from 'bruteLibrary.js'
-import {bruteForceHybrid} from 'bruteHybrid.js'
+import {generatePassword} from './passwordGenerator.js'
+import {generateConverter} from './passwordConverter,js'
+import {bruteForceSimple} from './bruteSimple.js'
+import {bruteForceLibrary} from './bruteLibrary.js'
+import {bruteForceHybrid} from './bruteHybrid.js'
 
 
 
