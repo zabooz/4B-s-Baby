4a60c8384f27201d859c70c45533ffd86e3dfeca
zabooz <zabooz1988@gmail.com>
2024-06-08 17:42:34 +0200

    blodfgh

--- client/scripts/bruteHybrid.js
+++ client/scripts/bruteHybrid.js
@@ -1,3 +1,80 @@
-export function bruteForceHybrid(){
+import { all } from "axios";
+
+export async function bruteForceHybrid(pwd){
+
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?'.split('');
+
+
+
+    const startTime = Date.now()
+    const normPwd = pwd.toLowerCase();
+
+    let pwdFound = false;
+    let time = 'N/A'
+    
+    let passwords;
+
+    try{
+        const response = await fetch('../data/rockYou.json')
+        passwords = await response.json()
+
+
+    }catch(err){
+        console.log(err, 'i fucked up')
+    }
+
+
+
+    if(passwords){
+
+        passwords.lines.forEach(pw => {
+        
+        const normPw = pw.toLowerCase()
+
+
+        if(pw === pwd){
+            pwdFound = true;
+            time = ((Date.now() - startTime)/1000) + ' sec';
+        }else if(normPw.includes(normPwd)){
+            
+            const startIndex = normPw.split('').indexOf(normPwd[0])
+            const endIndex = normPw.split('').lastIndexOf(normPwd[normPwd.length-1])
+
+
+
+        }
+    
+        
+    })
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
+
+
+function fixes (index){
+
+    let fix;
+
+    for (let i = 0; i < allCharacters.length;i++){
+
+        
+        fix.length < index ? fix += allCharacters[i]
+
+        const fix = allCharacters[i]
+
+
+    }
+
+
 
 }
