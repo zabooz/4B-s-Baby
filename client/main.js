128e74a08496133ca0747cc683943ed50977f2b3
Daniel Osojnik <zabooz1988@gmai.com>
2024-06-11 11:22:07 +0200

    sdg

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import {generatePassword} from './scripts/passwordGenerator.js'
 import {passwordConverter} from './scripts/passwordConverter.js'
-import {bruteForceHybrid} from './scripts/bruteHybrid.js'
+
 
 const convertButton = document.getElementById('convertButton')
 const startBtn = document.getElementById('startBtn');
@@ -14,58 +14,59 @@ convertButton.addEventListener('click', function() {
 })
 
 startBtn.addEventListener('click',(e)=> {
-    const bruteType = document.querySelector('input[name="bruteType"]:checked').value
-    const url = "http://localhost:3001/"
-    const pwd = document.getElementById('userPwd')
-    const urlPara =`${url}bruteforce${bruteType}?pwd=${encodeURIComponent(pwd.value)}`
-
-    pwd.value = ''
-    changeImg(e);
-  
-    let result;
-    
-    fetch(urlPara)
-        .then(response => {
-
-            if(!response.ok){
-                throw new Error('I fucked up again')
-            }
-
-            return response.json()
-
-        })
-        .then(data => {
-          result = data;
-          console.log(data);
-
-          updateAttempts(result)
-
-
-        })
-        .catch(error => {
-            console.error('Yeah nnonononono',error)
-        })
-
+  fetchData();
+  changeImg(e);
 })
 
 
 stopBtn.addEventListener('click',(e) => {
-
     changeImg(e)
 
-
 })
+
+
+const fetchData = () => {
+        const bruteType = document.querySelector(
+          'input[name="bruteType"]:checked'
+        ).value;
+        const url = "http://localhost:3001/";
+        const pwd = document.getElementById("userPwd");
+        const urlPara = `${url}bruteforce${bruteType}?pwd=${encodeURIComponent(
+          pwd.value
+        )}`;
+
+        pwd.value = "";
+        
+
+        let result;
+
+        fetch(urlPara)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("I fucked up again");
+            }
+
+            return response.json();
+          })
+          .then((data) => {
+            result = data;
+            updateAttempts(result);
+          })
+          .catch((error) => {
+            console.error("Yeah nnonononono", error);
+          });
+}
+
+
 
 
 
 function changeImg(e){
     const img = document.getElementById("monkey");
-
     img.src =
       e.target.id === "startBtn"
         ? "../img/animated_monkey.gif"
         : "../img/non_animated_monkey.png";
-
 }
 
 
@@ -78,6 +79,8 @@ function updateAttempts(result){
      para.innerHTML = `${pwd}  ${count}  ${closeEnough.length}  ${mode}  ${time}`
 
     stats.append(para)
+
+
 
 }
 
