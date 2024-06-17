c78b896d9f5d68602ca703bfe683523c2e3a3ef2
Daniel Osojnik <zabooz1988@gmai.com>
2024-06-17 11:54:01 +0200

    sdgsdg

--- server/server.js
+++ server/server.js
@@ -1,111 +1,91 @@
-
-import { config } from 'dotenv';
-import express from 'express'
-import axios from 'axios'
-import cors from 'cors'
-import{bruteForceSimple} from '../client/scripts/bruteSimple.js'
-import{bruteForceLibrary} from '../client/scripts/bruteLibrary.js'
-import { passwordDecoder } from '../client/scripts/encoder.js';
+import { config } from "dotenv";
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+import { bruteForceSimple } from "../client/scripts/bruteSimple.js";
+import { bruteForceLibrary } from "../client/scripts/bruteLibrary.js";
+import { passwordDecoder } from "../client/scripts/encoder.js";
 config();
 
 const app = express(); // Create an Express application
 const port = process.env.PORT || 3000; // Set the port from the environment variable or default to 3000
 const dropboxFileUrl = process.env.DROPBOX_FILE_URL; // Set the Dropbox file URL from the environment variable
 
-
-
 app.use(cors());
 
-let passwordList
+let passwordList;
 
-let requests = {}
-async function loadPasswordList(){
-
-    try{
-      const response = await axios.get(dropboxFileUrl, {
-        responseType: "text",
-      });
-      passwordList = response.data
-        .split("\n")
-        .filter((line) => line !== "");
-        console.log('data loaded')
-
-      
-    }catch(error){
-      console.error('dropbbox fail',error)
-      response.status(500).send('dropbox failure')
-    }
-
+let requests = {};
+async function loadPasswordList() {
+  try {
+    const response = await axios.get(dropboxFileUrl, {
+      responseType: "text",
+    });
+    passwordList = response.data.split("\n").filter((line) => line !== "");
+    console.log("data loaded");
+  } catch (error) {
+    console.error("dropbbox fail", error);
+    response.status(500).send("dropbox failure");
+  }
 }
 
-loadPasswordList()
+loadPasswordList();
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
 
-
-app.get('/bruteForceSimple', async(req,res) => {
-
-    const key = req.query.key
-    const password = req.query.pwd || 'abc'
-    const decodedPwd = passwordDecoder(password, key)
-    const maxLength = 16; 
-    const charset =
+app.get("/bruteForceSimple", async (req, res) => {
+  const key = req.query.key;
+  const password = req.query.pwd || "abc";
+  const decodedPwd = passwordDecoder(password, key);
+  const maxLength = 16;
+  const charset =
     'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:",.<>?/`~';
-    console.log(password,decodedPwd)
-    const requestId = req.query.requestId
+  console.log(password, decodedPwd);
+  const requestId = req.query.requestId;
 
-    requests[requestId] = true
-
-
-  try{
-    const result = await bruteForceSimple(decodedPwd,charset,maxLength, () => requests[requestId])
-    res.send(result)
-
-  }catch(error){
-    console.log(error)
-  }
-
-
-})
-
-
-app.get('/bruteForceLibrary',async(req,res) => {
-  
-  const password = req.query.pwd || 'abc'
-  const decodedPwd = passwordDecoder(password)
+  requests[requestId] = true;
 
   try {
-    
-    const result = await bruteForceLibrary(decodedPwd,passwordList)
-    res.send(result)
-    console.log(result)
-      }catch(error){
-        if(!passwordList) console.error('data not loaded')
-        console.error('Ah shit, here we go again')
-        res.status(500).send('error')
-      }
-        
-        
-})
-app.get('/bruteForceHybrid',async (req,res) => {
-    const key = req.query.key
-    const password = req.query.pwd || 'abc'
-    const decodedPwd = passwordDecoder(password, key)
+    const result = await bruteForceSimple(
+      decodedPwd,
+      charset,
+      maxLength,
+      () => requests[requestId]
+    );
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-    res.send('Not there yet')
-})     
+app.get("/bruteForceLibrary", async (req, res) => {
+  const password = req.query.pwd || "abc";
+  const decodedPwd = passwordDecoder(password);
 
+  try {
+    const result = await bruteForceLibrary(decodedPwd, passwordList);
+    res.send(result);
+    console.log(result);
+  } catch (error) {
+    if (!passwordList) console.error("data not loaded");
+    console.error("Ah shit, here we go again");
+    res.status(500).send("error");
+  }
+});
+app.get("/bruteForceHybrid", async (req, res) => {
+  const key = req.query.key;
+  const password = req.query.pwd || "abc";
+  const decodedPwd = passwordDecoder(password, key);
 
+  res.send("Not there yet");
+});
 
-app.get('/stopbruteforce',(req,res) => {
-
-    const requestId = req.query.requestId
-    requests[requestId] = false
-})
-
-
+app.get("/stopbruteforce", (req, res) => {
+  const requestId = req.query.requestId;
+  requests[requestId] = false;
+});
 
 // Start the server and log the URL to the console
 app.listen(port, () => {
