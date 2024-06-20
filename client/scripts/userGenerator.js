802d07d739158234621ca8251d9c4f682e573bf9
Daniel Osojnik <zabooz1988@gmai.com>
2024-06-20 12:56:51 +0200

    sdg

--- client/scripts/userGenerator.js
+++ client/scripts/userGenerator.js
@@ -1,12 +1,12 @@
-import {colors, shapes, textures, sizes, emotions, tastes, sounds, temperatures, speeds, qualities, weatherTypes, instruments, fruits, animals, fantasyCreatures, vegetables, rpgClasses, fantasyRaces, occupations, vehicles, sports, tools, beverages, clothingItems, bodyParts, flowers, desserts} from './generatorData.js'
+import {colors, shapes, textures, sizes, emotions, tastes, sounds, temperatures, speeds, qualities, weatherTypes, instruments, fruits, animals, fantasyCreatures, vegetables, rpgClasses, fantasyRaces, occupations, vehicles, sports, tools, beverages, clothingItems, bodyParts, flowers, desserts} from './generator.Data.js'
 import { generateEncodingKey } from './scripts/encoder.js'
-//Generates a user name based on select input
+
 
 function generateUser(adjective1, adjective2, selectedNoun) {
     let newUser = "";
-    console.log(colors)
+
     let randomNumber = generateEncodingKey()
-    const adjectivesObj {
+    const adjectivesObj  = {
         colors: colors,
 
 
