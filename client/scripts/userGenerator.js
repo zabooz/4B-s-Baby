fde44b369c93eef74004c0da376121d88f9d9cd1
Zappls <167064711+Zappls@users.noreply.github.com>
2024-06-24 08:08:07 +0000

    userGenerator | added duplicate result check

--- client/scripts/userGenerator.js
+++ client/scripts/userGenerator.js
@@ -8,7 +8,6 @@ export function generateUser(adjective1, adjective2, selectedNoun) {
     let newUser = "";
     let randomAdjective1, randomAdjective2, randomNoun;
     
-    
     const myArraysObj = {
         colors: colors,
         shapes: shapes,
@@ -37,51 +36,54 @@ export function generateUser(adjective1, adjective2, selectedNoun) {
         bodyParts: bodyParts, 
         flowers: flowers, 
         desserts: desserts
-    }
+    };
     let selectedAdjectiveArray, selectedNounArray;
 
-
+    // Function to generate a unique word from the array
+    function getUniqueRandomWord(array, ...existingWords) {
+        let randomWord;
+        do {
+            randomWord = array[generateEncodingKey(array)];
+        } while (existingWords.includes(randomWord));
+        return randomWord;
+    }
 
     // Determine which array to select based on adjective1
     if (myArraysObj.hasOwnProperty(adjective1)) {
         selectedAdjectiveArray = myArraysObj[adjective1];
-        randomAdjective1 = selectedAdjectiveArray[generateEncodingKey(selectedAdjectiveArray)];
-        
-    } 
-    else if(adjective1===""){
+        randomAdjective1 = getUniqueRandomWord(selectedAdjectiveArray);
+    } else if(adjective1 === "") {
         randomAdjective1 = "";
-    }
-    else {
-        // Handle default case or error if needed
+    } else {
         console.error(`Array for adjective1 "${adjective1}" not found.`);
         return;
     }
+
     // Determine which array to select based on adjective2
     if (myArraysObj.hasOwnProperty(adjective2)) {
         selectedAdjectiveArray = myArraysObj[adjective2];
-        randomAdjective2 = selectedAdjectiveArray[generateEncodingKey(selectedAdjectiveArray)];
-        
-    } else if(adjective2===""){
+        randomAdjective2 = getUniqueRandomWord(selectedAdjectiveArray, randomAdjective1);
+    } else if(adjective2 === "") {
         randomAdjective2 = "";
     } else {
-        // Handle default case or error if needed
         console.error(`Array for adjective2 "${adjective2}" not found.`);
         return;
     }
+
     // Determine which array to select based on noun
     if (myArraysObj.hasOwnProperty(selectedNoun)) {
         selectedNounArray = myArraysObj[selectedNoun];
-        randomNoun = selectedNounArray[generateEncodingKey(selectedNounArray)];
-        
-    }  else {
-        // Handle default case or error if needed
+        randomNoun = getUniqueRandomWord(selectedNounArray, randomAdjective1, randomAdjective2);
+    } else {
         console.error(`Array for noun "${selectedNoun}" not found.`);
         return;
     }
+
     newUser = `${randomAdjective1}${randomAdjective2}${randomNoun}`;
     console.log(newUser);
     return newUser;
 }
+
 
 
 
