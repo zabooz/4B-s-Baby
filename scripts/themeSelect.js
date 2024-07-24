ca710030fceff99052bbacb7da6581a8e3b38db6
Daniel Osojnik <zabooz1988@gmai.com>
2024-07-24 08:32:36 +0200

    fixed wrong logo matrix theme mobile

--- scripts/themeSelect.js
+++ scripts/themeSelect.js
@@ -5,4 +5,5 @@ export function changeTheme(theme, logo) {
     reset();
     document.getElementById('themeStylesheet').setAttribute('href', theme);
     document.getElementById('logo').setAttribute('src', logo);
+    document.getElementById('logoFooter').setAttribute('src', logo);
 }
