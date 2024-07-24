abdabf42564ff1ed42ff302d0ea0de3dda7f5f29
Daniel Osojnik <zabooz1988@gmai.com>
2024-07-24 10:02:37 +0200

    added confirm bubbles for clipboard

--- scripts/copybutton.js
+++ scripts/copybutton.js
@@ -3,6 +3,12 @@ export const copyButton = (textId) => {
     const button = document.createElement('button')
     button.className = 'copyButton'
     
+    const confirmBubble = document.createElement('div')
+    confirmBubble.className = 'confirmBubble'
+    confirmBubble.innerText = 'Copied!'
+
+
+
     const img = document.createElement('img')
 
     const boolean = document.getElementById('themeStylesheet').getAttribute('href').includes('serious')
@@ -14,13 +20,18 @@ export const copyButton = (textId) => {
         img.src = "/img/copyToClipBoard2.png"
     }
 
-    button.append(img)
+    button.append(img, confirmBubble)
     const textElement = document.getElementById(textId)
     const index = textElement.innerText.lastIndexOf(' ') 
     const text = textElement.innerText.slice(index)
 
     button.addEventListener('click',() => {
         navigator.clipboard.writeText(text)
+
+        confirmBubble.classList.add('fadeIn')
+        setTimeout(() => {
+            confirmBubble.classList.remove('fadeIn')
+        }, 2000);
     })
 
     return button
