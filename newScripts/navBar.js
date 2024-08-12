60ac54a85ef16fee2311c9af14e0e02ba8a5802f
Daniel Osojnik <zabooz1988@gmai.com>
2024-08-12 14:58:49 +0200

    added navbar to new Design

---
+++ newScripts/navBar.js
@@ -0,0 +1,63 @@
+const config = {
+  testing: {
+    text: "Password Testing",
+    link: "#",
+  },
+  userGen: {
+    text: "User Generator",
+    link: "#",
+  },
+  pwGen: {
+    text: "Password Generator",
+    link: "#",
+  },
+  logo: {
+    text: "Mayo Monkeys",
+    link: "./img/animated_monkey.gif",
+  },
+};
+
+createNavBar = (config) => {
+  const navBar = `
+    <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
+    <div class="d-flex align-items-center">
+    <img src=${config.logo.link} />
+    <p class="navbar-brand mb-0 ms-3" href="#">${config.logo.text}</p>
+  </div>
+  <button class="navbar-toggler ms-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ms-auto">
+      <li class="nav-item">
+        <a class="nav-link" href=${config.testing.link}>${config.testing.text}</a>
+      </li>
+            <li class="nav-item">
+        <a class="nav-link" href=${config.userGen.link}>${config.userGen.text}</a>
+      </li>      <li class="nav-item">
+        <a class="nav-link" href=${config.pwGen.link}>${config.pwGen.text}</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Language
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">English</a>
+          <a class="dropdown-item" href="#">German</a>
+          <a class="dropdown-item" href="#">Spain</a>
+          <a class="dropdown-item" href="#">France</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./oldDesign/index.html">Old Design</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    `;
+
+  return navBar;
+};
+
+document.querySelector("header").innerHTML = createNavBar(config);
