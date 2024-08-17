6d53b00a87eb17b709bcbd13dc2b6c601ef09b21
zaboo <zabooz1988@gmail.com>
2024-08-17 14:02:01 +0200

    path

--- components/navBar.js
+++ components/navBar.js
@@ -3,23 +3,23 @@ const configNav = {
   targetId: "nav",
 
   home: {
-    link: "../index.html",
+    link: "./index.html",
   },
   testing: {
     text: "Password Testing",
-    link: "../featureHtmls/testing.html",
+    link: "./featureHtmls/testing.html",
   },
   userGen: {
     text: "User Generator",
-    link: "../featureHtmls/userGenerating.html",
+    link: "./featureHtmls/userGenerating.html",
   },
   pwGen: {
     text: "Password Generator",
-    link: "../featureHtmls/pwGenerating.html",
+    link: "./featureHtmls/pwGenerating.html",
   },
   logo: {
     text: "Password Playground",
-    link: "../img/animated_monkey.gif",
+    link: "./img/animated_monkey.gif",
   },
 };
 
