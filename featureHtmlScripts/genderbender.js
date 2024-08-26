ed9b37f3e0d6bab5bd3c19f1138337b1ee23653b
Zappls <167064711+Zappls@users.noreply.github.com>
2024-08-26 13:39:07 +0000

    working on it

--- featureHtmlScripts/genderbender.js
+++ featureHtmlScripts/genderbender.js
@@ -2,7 +2,32 @@ import { S } from "xmlchars/xml/1.0/ed5.js";
 import { aiApiCallUsername } from "../utilities/aiApiCallUsername.js";
 import { rndNumInLen } from "../scripts/encoder.js";
 
-const genderlessEndings = [];
+const genderlessEndings = [
+  "matik", // e.g., Automatik, Mathematik
+  "nautik", // e.g., Astronautik, Aeronautik
+  "omatisch", // e.g., systematisch, automatisch
+  "logisch", // e.g., biologisch, psychologisch
+  "graphie", // e.g., Geographie, Astronomie
+  "dynamik", // e.g., Dynamik, Kinematik
+  "therapie", // e.g., Therapie, Physiotherapie
+  "technologie", // e.g., Technologie, Biotechnologie
+  "onomie", // e.g., Astronomie, Ökonomie
+  "chemie", // e.g., Chemie, Biochemie
+  "logie", // e.g., Geologie, Psychologie
+  "urgie", // e.g., Chirurgie, Pathologie
+  "istik", // e.g., Linguistik, Systematik
+  "graphik", // e.g., Grafik, Kartographie
+  "phie", // e.g., Philosophie, Geographie
+  "metrie", // e.g., Geometrie, Astrometrie
+  "konomie", // e.g., Ökonomie, Mikrokonomie
+  "iatrie", // e.g., Psychiatrie, Pädiatrie
+  "kologie", // e.g., Biologie, Ökologie
+  "physik", // e.g., Physik, Biophysik
+  "biologie", // e.g., Biologie, Mikrobiologie
+  "analyse", // e.g., Analyse, Datenanalyse
+  "struktur", // e.g., Struktur, Mikrostruktur
+  "theorie", // e.g., Theorie, Physiktheorie
+];
 
 function addEndings(object) {
   const pattern = /[aeiou]er$/;
@@ -73,7 +98,7 @@ export async function genderbend(username) {
   let apiString = adj1 + " " + adj2 + " " + noun;
   console.log(apiString);
 
-  const sysContent = `I want you to translate ${apiString} to german. For the adjectives in ${apiString} I want you to return the german root word only. I want you to return the result as a JSON object where the key is the gender of the noun in ${apiString} Example: For "crazy red cucumber" the result I'm looking for would be a JSON object in the form of {"female": ["verückt", "rot", "Gurke"]}. The response should be the resulting object only.`;
+  const sysContent = `I want you to translate ${apiString} to german. I want you to return the german root word only. I want you to return the result as a JSON object where the key is the gender of the noun in ${apiString} Example: For "crazy red cucumber" the result I'm looking for would be a JSON object in the form of {"female": ["verückt", "rot", "Gurke"]}. The response should be the resulting object only.`;
 
   try {
     const apiResultString = await aiApiCallUsername(apiString, sysContent);
