3eafd89ae5d2ec9187a974c03609dd59a466b79a
Daniel Osojnik <zabooz1988@gmai.com>
2024-08-28 06:24:40 +0200

    switched position navbar clippy <--> ai

--- components/navBar.js
+++ components/navBar.js
@@ -68,20 +68,18 @@ const createNavBar = (configNav) => {
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto d-flex justify-content-end px-5 w-100">
+        <li class="nav-item">
+        <a class="nav-link" href="${configNav.pwGen.link}">${configNav.pwGen.text}</a>
+        </li>
           <li class="nav-item">
             <a class="nav-link" href="${configNav.testing.link}">${configNav.testing.text}</a>
           </li>
           <li class="nav-item">
-          <a class="nav-link" href="${configNav.pwGen.link}">${configNav.pwGen.text}</a>
-          </li>
-          <li class="nav-item">
             <a class="nav-link" href="${configNav.userGen.link}">${configNav.userGen.text}</a>
           </li>
+
             <li class="nav-item">
             <a class="nav-link text-danger" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal" >Clippy</a>
-          </li>
-            <li class="nav-item">
-            <a class="nav-link text-danger" href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">Ai Assistant</a>
           </li>
             </li>
             <li class="nav-item">
