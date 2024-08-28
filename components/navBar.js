3696105b1bf42158f0e3e523b7d5c2b2a4c1e4d4
Daniel Osojnik <zabooz1988@gmai.com>
2024-08-28 13:11:38 +0200

    responsivness navbar

--- components/navBar.js
+++ components/navBar.js
@@ -59,7 +59,7 @@ const createNavBar = (configNav) => {
     <nav class="navbar navbar-expand-md nav-custom fw-semibold text-black">
       <div>
         <a href="${configNav.home.link}" class="d-flex align-items-center" style="text-decoration: none;">
-          <img src="${configNav.logo.link}" alt="logo" class="d-none d-sm-block" />
+          <img src="${configNav.logo.link}" alt="logo" class="d-none d-lg-block" />
           <p class="navbar-brand mb-0 ms-3">${configNav.logo.text}</p>
         </a>
       </div>
@@ -67,7 +67,7 @@ const createNavBar = (configNav) => {
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto d-flex justify-content-end px-5 w-100">
+        <ul class="navbar-nav  d-flex justify-content-end w-100">
         <li class="nav-item">
         <a class="nav-link" href="${configNav.pwGen.link}">${configNav.pwGen.text}</a>
         </li>
@@ -78,8 +78,8 @@ const createNavBar = (configNav) => {
             <a class="nav-link" href="${configNav.userGen.link}">${configNav.userGen.text}</a>
           </li>
 
-<li class="nav-item">
-    <a class="nav-link text-danger" href="#" data-bs-toggle="offcanvas" data-bs-target="#clippy" role="button" aria-controls="clippy">Clippy</a>
+<li class="nav-item d-flex align-items-center">
+    <a class="nav-link text-danger d-flex align-items-center" href="#" data-bs-toggle="offcanvas" data-bs-target="#clippy" role="button" aria-controls="clippy">Clippy</a>
 </li>
 
             <li class="nav-item">
