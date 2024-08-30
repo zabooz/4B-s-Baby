74aec4ca3e1c77ab79e1a1377f6dbf05974d269a
Daniel Osojnik <zabooz1988@gmai.com>
2024-08-30 10:04:35 +0200

    maybe final ?

--- components/heroText.js
+++ components/heroText.js
@@ -7,19 +7,23 @@ const createHero = (content) => {
     <div  id="jumbotron" class="d-flex flex-column gap-2 align-items-center">
         
     <div class="w-100 d-flex justify-content-center  flex-column pt-2">
-    <div class="d-flex justify-content-lg-start justify-content-center gap-5 mb-5 me-5">
-    <img  id="PPIcon"src="./img/landingPage/PPicon.png" class="mx-5 img-fluid">
-    
-    <img id="padlockPic" src="./img/landingPage/padlock.svg" class"img-fluid d-none mx-5 " >
+    <div  id="logoContainer"  class="d-flex justify-content-lg-start justify-content-center gap-5 mb-5 me-5">
+    <div class="w-50 pt-5">
+    <img  id="PPIcon"src="./img/landingPage/PPicon.png" class="px-5 img-fluid">
     </div>
-    <p  class="lead w-75  text-center pb-2 fs-2 ms-5 text-lg-start border-bottom border-2 border-dark fw-semibold ">${content.lead}</p>
-    
+    <div class="w-25 pt-5">
+    <img id="padlockPic" src="./img/landingPage/padlock.svg" class"img-fluid d-none mx-5 my-auto" >
     </div>
-    <div  class="w-100  d-flex justify-content-center ms-5 flex-column"> 
-    <p class="w-75 fs-3 ms-4">
+    </div>
+    <div class="d-flex">
+    <p  class="lead   text-center pb-2 fs-2 ms-5 text-lg-start border-bottom border-2 border-dark fw-semibold ">${content.lead}</p>
+    </div>
+    </div>
+    <div  class="w-75 d-flex me-auto ms-4 flex-column"> 
+    <p class="w-50 fs-4 ms-4">
     ${content.p}
     </p>
-    <p class=" w-75  mt-3 ms-4 fs-3 ">
+    <p class=" w-50  mt-3 ms-4 fs-4 ">
     ${content.p2}
     </p>
     </div>
