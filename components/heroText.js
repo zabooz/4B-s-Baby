55546516c8404b161efe31cc1c1d3dc9fdccef8a
zaboo <zabooz1988@gmail.com>
2024-08-30 16:13:04 +0200

    landing even more responsive

--- components/heroText.js
+++ components/heroText.js
@@ -6,26 +6,26 @@ const createHero = (content) => {
   <div id="jumbotron" class="d-flex w-100 flex-column gap-4 align-items-center p-3">
       
       <div class="container-fluid row d-flex justify-content-center flex-column pt-2">
-          <div id="logoContainer" class="row col-12 b align-items-center gap-5 mb-5">
-              <div class="col-12 col-xxl-3    col-xl-3 pt-xxl-5 pt-xl-4 text-center text-md-start">
+          <div id="logoContainer" class="row col-12 b justify-content-center justify-content-sm-center  justify-content-md-start justify-content-sm-start gap-5 mb-5">
+              <div class="col-10 col-xxl-3 col-sm-8 col-lg-4  col-xl-3 col-md-4 pt-xxl-5 pt-xl-4 text-center text-md-start">
                   <img id="PPIcon" src="./img/landingPage/PPicon.png" class="img-fluid pt-xxl-4">
               </div>
-              <div class="col-12 col-xxl-2 col-xl-2 pt-xl-0 text-center text-md-start d-none d-md-block">
+              <div class="col-12 col-xxl-2 col-xl-2 col-lg-3 col-md-3 pt-xxl-5 pt-xl-4 text-center text-md-start d-none d-md-block">
                   <img id="padlockPic" src="./img/landingPage/padlock.svg" class="img-fluid">
               </div>
           </div>
-          <div  class=" container-fluid d-flex">
-              <p id="leadText" class="lead  fs-1  text-center text-md-start fs-3 pb-2 ps-0 border-bottom border-2 border-dark fw-semibold">${content.lead}</p>
+          <div  class=" container-fluid d-flex justify-content-center justify-content-md-start">
+              <p id="leadText" class="lead  fs-1  text-center text-md-center fs-3 pb-2 ps-0 border-bottom border-2 border-dark fw-semibold">${content.lead}</p>
           </div>
       </div>
       
-      <div class="container-fluid d-flex flex-column mt-xxl-4 ms-3"> 
-          <p  class=" heroText col-12 col-xxl-5 col-xl-5 fs-4  text-center text-md-start">${content.p}</p>
-          <p class=" heroText col-12 col-xxl-5 col-xl-5 fs-4 mt-3 text-center text-md-start">${content.p2}</p>
+      <div class="container-fluid d-flex flex-column mt-xxl-4 ms-sm-3"> 
+          <p  class=" heroText col-12 col-xxl-5 col-xl-5 col-lg-5 fs-4 col-md-7  text-center text-md-start">${content.p}</p>
+          <p class=" heroText col-12 col-xxl-5 col-xl-5 fs-4 mt-3 col-lg-5 col-md-7  text-center text-md-start">${content.p2}</p>
       </div>
       
-      <a href="#spanScroll" id="test" class="mt-xxl-5 mt-3">
-          <img id="arrow" src="./img/landingPage/arrow-pointing.svg" class="img-fluid me-5 ">
+      <a href="#spanScroll" id="test" class="mt-auto">
+          <img id="arrow" src="./img/landingPage/arrow-pointing.svg" class="img-fluid me-sm-5 ">
       </a>
   </div>
   `;
