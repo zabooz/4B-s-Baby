4d3a5dd9057d66fe9dc584c762559c9c9e6e0152
zaboo <zabooz1988@gmail.com>
2024-08-31 22:56:39 +0200

    enahnced accessibility

--- components/quickNav.js
+++ components/quickNav.js
@@ -25,12 +25,12 @@ const quickNav = (config) => {
     <div id="quickNav" class="d-sm-flex d-none flex-column gap-1 ">
 
     <button class="btn btn-primary d-md-flex d-none align-items-center gap-2" id="aiAssistant" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-        <img src = ${config.ai.img} >
+        <img src = ${config.ai.img}  alt="Ai-Assistant">
         <p class="mb-0">${config.ai.text}
             </button>
 
         <button type="button" class="btn btn-primary btn btn-primary d-md-flex d-none align-items-center gap-2" data-bs-toggle="offcanvas" data-bs-target="#clippy" role="button" aria-controls="clippy">
-                    <img src = ${config.clippy.img}>
+                    <img src = ${config.clippy.img} alt="Clippy">
         <p class="mb-0">${config.clippy.text}
         </button
 
