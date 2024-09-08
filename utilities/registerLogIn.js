2be0debae399d582078e415779da8c45ad0b8f7b
zaboo <zabooz1988@gmail.com>
2024-09-08 22:28:40 +0200

    added validation

--- utilities/registerLogIn.js
+++ utilities/registerLogIn.js
@@ -114,3 +114,74 @@ export const deleteUser = async () => {
    
   }
 };
+
+
+// ======================   VALdiation   ======================
+
+
+
+export const validateRegisterForm = () => {
+  const email = document.getElementById("validationDefault02");
+  const confirmEmail = document.getElementById("validationDefault03");
+  const password = document.getElementById("validationDefault05");
+  const confirmPassword = document.getElementById("validationDefault04");
+
+  let isValid = true;
+
+  // Überprüfung, ob E-Mail-Adressen übereinstimmen
+  if (email.value !== confirmEmail.value) {
+    confirmEmail.setCustomValidity("E-Mail-Adressen stimmen nicht überein.");
+    confirmEmail.classList.add("is-invalid");
+    isValid = false;
+  } else {
+    confirmEmail.setCustomValidity("");
+    confirmEmail.classList.remove("is-invalid");
+    confirmEmail.classList.add("is-valid");
+  }
+
+  // Überprüfung, ob Passwörter übereinstimmen
+  if (password.value !== confirmPassword.value || password.value === "") {
+    confirmPassword.setCustomValidity("Passwörter stimmen nicht überein.");
+    confirmPassword.classList.add("is-invalid");
+    isValid = false;
+  } else {
+    confirmPassword.setCustomValidity("");
+    confirmPassword.classList.remove("is-invalid");
+    confirmPassword.classList.add("is-valid");
+  }
+
+  return isValid;
+};
+
+
+
+  // Dynamische Überprüfung der Eingabefelder während des Tippens
+  export const addDynamicValidation = (email,confirmEmail,password,confirmPassword,registerForm) => {
+
+  
+    // Überprüfung der E-Mail Felder während der Eingabe
+    email.addEventListener("input", () => validateRegisterForm());
+    confirmEmail.addEventListener("input", () => validateRegisterForm());
+  
+    // Überprüfung der Passwort Felder während der Eingabe
+    password.addEventListener("input", () => validateRegisterForm());
+    confirmPassword.addEventListener("input", () => validateRegisterForm());
+
+  
+  // Event-Listener für Formular-Validierung
+  registerForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    // Rufe die Validierungsfunktion auf
+    if (validateRegisterForm()) {
+      const username = document.getElementById("validationDefault01").value;
+      const email = document.getElementById("validationDefault02").value;
+      const password = document.getElementById("validationDefault05").value;
+  
+      // Führe den Registrierungsvorgang durch, wenn die Validierung erfolgreich war
+      register(username, password, email);
+    }
+  });
+  
+};
+
